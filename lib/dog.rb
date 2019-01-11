aa69b167be8d91470be4dad6a08ef15da3dbac5c
Elana Ryznar <eryzna@gmail.com>
2019-01-11 02:28:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,6 +57,13 @@ class Dog
     end.first
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
 
 
 
